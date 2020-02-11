f1602276c4c54380eceb72d008577b46eafc7e85
Rasgacode <rongybaba12@gmail.com>
2020-02-11 09:27:45 +0100

    exercise 5

---
+++ SI1/Person/SortString/Program.cs
@@ -0,0 +1,17 @@
+﻿using Person;
+using System;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split();
+            Array.Sort(words);
+            str = string.Join(" ", words);
+            Console.WriteLine(str);
+        }
+    }
+}

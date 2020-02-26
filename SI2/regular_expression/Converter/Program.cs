decf14f3ff8cdbba4b1eb8ba7f7c0e482ca6e221
Rasgacode <rongybaba12@gmail.com>
2020-02-26 14:16:33 +0100

    Implement converter task

---
+++ SI2/regular_expression/Converter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            var sr = new StreamReader("D:/CC_C#_branch/C-_SI_assingments/SI2/regular_expression/Converter/file.txt", true);
+            text = sr.ReadToEnd();
+            sr.Close();
+            Encoding e = Encoding.UTF7;
+            byte[] encoded = e.GetBytes(text);
+            File.WriteAllBytes("D:/CC_C#_branch/C-_SI_assingments/SI2/regular_expression/Converter/file.txt", encoded);
+        }
+    }
+}
